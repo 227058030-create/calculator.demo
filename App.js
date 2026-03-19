9228f01bb101f637edd4efadfa10f025b028b9fb
227058030-create <227058030@sastra.ac.in>
2026-03-19 16:40:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,81 @@
+import './App.css' 
+import React from 'react'; 
+class App extends React.Component 
+{ 
+constructor(props) 
+{ 
+super(props); 
+this.state = {displayValue: "",}; 
+} 
+handleButtonClick = (value) => 
+{ 
+if (value === "=") 
+{ 
+this.calculateResult(); 
+} 
+else if (value === "c") 
+{ 
+this.clearDisplay(); 
+} 
+else 
+{ 
+this.setState((prevState) => ( 
+{ 
+displayValue: prevState.displayValue + value, 
+}) 
+); 
+} 
+}; 
+calculateResult = () => 
+{ 
+try 
+{ 
+  const result = eval(this.state.displayValue); 
+  this.setState({ displayValue: result }); 
+} 
+catch (error) 
+{ 
+this.setState({ displayValue: "Error" }); 
+} 
+finally
+{
+
+}
+}; 
+clearDisplay = () => 
+{ 
+this.setState({ displayValue: "", 
+}); 
+}; 
+render() 
+{ 
+return( 
+<> 
+<h1>Calculator</h1> 
+<div className="calculator"> 
+<div className="display">{this.state.displayValue}</div> 
+<div className="buttons"> 
+<button onClick={() => this.handleButtonClick('7')}>7</button> 
+<button onClick={() => this.handleButtonClick('8')}>8</button> 
+<button onClick={() => this.handleButtonClick('9')}>9</button> 
+<button onClick={() => this.handleButtonClick('/')}className='symbol'>/</button> 
+<button onClick={() => this.handleButtonClick('4')}>4</button> 
+<button onClick={() => this.handleButtonClick('5')}>5</button> 
+<button onClick={() => this.handleButtonClick('6')}>6</button> 
+<button onClick={() => this.handleButtonClick('*')} className='symbol'>*</button> 
+<button onClick={() => this.handleButtonClick('1')}>1</button> 
+<button onClick={() => this.handleButtonClick('2')}>2</button> 
+<button onClick={() => this.handleButtonClick('3')}>3</button> 
+<button onClick={() => this.handleButtonClick('-')} className='symbol'>-</button> 
+<button onClick={() => this.handleButtonClick('0')}>0</button> 
+<button onClick={() => this.handleButtonClick('.')}>.</button> 
+<button onClick={this.clearDisplay} className='ClearButton'>C</button> 
+<button onClick={() => this.handleButtonClick('+')}className='symbol'>+</button> 
+<button onClick={() => this.handleButtonClick('=')} className="equals">=</button> 
+</div> 
+</div> 
+</> 
+); 
+} 
+} 
+export default App;
